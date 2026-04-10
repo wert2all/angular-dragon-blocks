a3eaaed3d5ad665778a2521c1fa852f360a2524e
Serhii Shvets <wert2all@gmail.com>
2026-04-10 14:53:27 +0300

    chore: format files

--- eslint.config.js
+++ eslint.config.js
@@ -1,68 +1,68 @@
 // @ts-check
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
-    "files": ["src/**/*.ts"],
-    "ignores": ["src/**/*.spec.ts"],
-    "languageOptions": {
-      "parser": tsParser,
-      "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.app.json"
+    files: ['src/**/*.ts'],
+    ignores: ['src/**/*.spec.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.app.json',
       },
-      "globals": {
-        "browser": true,
-        "es2020": true,
-        "node": true
-      }
+      globals: {
+        browser: true,
+        es2020: true,
+        node: true,
+      },
     },
-    "plugins": {
-      "@typescript-eslint": tsPlugin
+    plugins: {
+      '@typescript-eslint': tsPlugin,
     },
-    "rules": {
-      "@typescript-eslint/no-unused-vars": ["error", {"args": "all", "argsIgnorePattern": "^_"}]
-    }
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+    },
   },
   {
-    "files": ["src/**/*.spec.ts"],
-    "languageOptions": {
-      "parser": tsParser,
-      "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.spec.json"
+    files: ['src/**/*.spec.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.spec.json',
       },
-      "globals": {
-        "browser": true,
-        "es2020": true,
-        "node": true,
-        "vitest": true
-      }
+      globals: {
+        browser: true,
+        es2020: true,
+        node: true,
+        vitest: true,
+      },
     },
-    "plugins": {
-      "@typescript-eslint": tsPlugin
+    plugins: {
+      '@typescript-eslint': tsPlugin,
     },
-    "rules": {
-      "@typescript-eslint/no-unused-vars": ["error", {"args": "all", "argsIgnorePattern": "^_"}]
-    }
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }],
+    },
   },
   {
-    "files": ["**/*.js", "**/*.cjs", "**/*.mjs"],
-    "languageOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module",
-      "globals": {
-        "browser": true,
-        "es2020": true,
-        "node": true
-      }
+    files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        es2020: true,
+        node: true,
+      },
     },
-    "rules": {
-      "no-unused-vars": "warn",
-      "no-undef": "error"
-    }
-  }
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+    },
+  },
 ];
