b40122d2ac81d7ad5fb19c16f22fa6822e8c4bc5
Serhii Shvets <wert2all@gmail.com>
2026-04-10 18:53:17 +0300

    feat(header): add volume toggle button with icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
+import { hugeVolumeHigh, hugeVolumeMute02 } from '@ng-icons/huge-icons';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideNgIconsConfig({
+      size: '1em',
+    }),
+    provideIcons({ hugeVolumeHigh, hugeVolumeMute02 }),
+  ],
 };
