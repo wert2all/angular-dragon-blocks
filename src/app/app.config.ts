671ec77f9474292293e0172c7d12c2571f5b18cd
Serhii Shvets <wert2all@gmail.com>
2026-04-10 23:01:38 +0300

    feat(deps): add @ngrx/store support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,16 @@ import { provideRouter } from '@angular/router';
 import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
 import { hugeCompass, hugeVolumeHigh, hugeVolumeMute02 } from '@ng-icons/huge-icons';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideNgIconsConfig({
-      size: '1em',
+        size: '1em',
     }),
     provideIcons({ hugeVolumeHigh, hugeVolumeMute02, hugeCompass }),
-  ],
+    provideStore()
+],
 };
