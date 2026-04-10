33a4e1c2c48f87717066a0f7862b455cdbbc4c52
Serhii Shvets <wert2all@gmail.com>
2026-04-10 16:29:56 +0300

    fix: fix header

--- src/app/layout/game-content.ts
+++ src/app/layout/game-content.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-game-content',
   imports: [],
   template: `
-    <div class="w-480 h-270 min-w-480 min-h-270 max-w-480 max-h-270">
+    <div
+      class="w-480 h-270 min-w-480 min-h-270 max-w-480 max-h-270 border-4 border-[#fa891a] shadow-[0_0_20px_0_#fa891a]"
+    >
       <ng-content />
     </div>
   `,
