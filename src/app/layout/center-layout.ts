090b566c68b4004a3ffa092d7491583811a64af0
Serhii Shvets <wert2all@gmail.com>
2026-04-10 18:18:50 +0300

    fix: fix app layout

--- src/app/layout/center-layout.ts
+++ src/app/layout/center-layout.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: `
-    <div class="flex h-dvh w-dvw items-center justify-center pt-20">
+    <div class="px-24 mt-24">
       <ng-content />
     </div>
   `,
