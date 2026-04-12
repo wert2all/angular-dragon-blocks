e8b7f67c766477dc6608da38070dcc3fccb45ff1
Serhii Shvets <wert2all@gmail.com>
2026-04-12 21:48:10 +0300

    feat: show quest details

--- src/app/features/quest/components/task/task.ts
+++ src/app/features/quest/components/task/task.ts
@@ -1,11 +1,28 @@
-import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+} from "@angular/core";
+import { Store } from "@ngrx/store";
+import { questFeature } from "../../store/quest.feature";
+import { QuestDetails } from "../details/details";
 
 @Component({
   selector: "app-quest-task",
-  imports: [],
+  imports: [QuestDetails],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: "./task.html",
 })
 export class TaskComponent {
+  private store = inject(Store);
   taskId = input.required<number>();
+
+  protected all = this.store.selectSignal(questFeature.selectKidQuests);
+
+  protected quest = computed(() => {
+    const questId = this.taskId();
+    return this.all().quests.filter((quest) => quest.id == questId)[0];
+  });
 }
