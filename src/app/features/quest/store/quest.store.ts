5c842a476441b39c15f9315fd7be33702a89cf50
Serhii Shvets <wert2all@gmail.com>
2026-04-12 15:30:22 +0300

    feat: show detail of quest on map marker click

--- src/app/features/quest/store/quest.store.ts
+++ src/app/features/quest/store/quest.store.ts
@@ -15,4 +15,18 @@ export const quests: Quest[] = [
     isDone: false,
     position: { x: 17, y: 78 },
   },
+  {
+    id: 3,
+    image: "/assets/quests/quest-3.png",
+    syllables: ["да", "вид"],
+    isDone: false,
+    position: { x: 23, y: 70 },
+  },
+  {
+    id: 4,
+    image: "/assets/quests/quest-4.png",
+    syllables: ["сир"],
+    isDone: false,
+    position: { x: 26, y: 67 },
+  },
 ];
---
+++ src/app/pages/map/components/quest-detail/quest-detail.component.ts
@@ -0,0 +1,22 @@
+import type { ViewQuestMarker } from "../../../../features/quest/store/quest.types";
+import { ChangeDetectionStrategy, Component, inject, input, output } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-quest-detail",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: "./quest-detail.component.html",
+  styleUrl: "./quest-detail.component.css"
+})
+export class QuestDetailComponent {
+  readonly quest = input.required<ViewQuestMarker | null>();
+  readonly close = output<void>();
+  private router = inject(Router);
+
+  onWordClick() {
+    const q = this.quest();
+    if (q) {
+      this.router.navigate(['/quest', q.id]);
+    }
+  }
+}
