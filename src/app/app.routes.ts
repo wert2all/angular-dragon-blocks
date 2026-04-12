7e8715a083ff160afef5eb1528b9aa85e0f4c29d
Serhii Shvets <wert2all@gmail.com>
2026-04-12 15:40:42 +0300

    fix: make map page as index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'map' },
   {
     path: 'map',
-    loadComponent: () =>
-      import('./pages/map/map-page.component').then((m) => m.MapPageComponent),
+    loadComponent: () => import('./pages/map/map-page.component').then((m) => m.MapPageComponent),
   },
   {
     path: 'quest/:id',
-    loadComponent: () =>
-      import('./pages/quest/quest').then((m) => m.Quest),
+    loadComponent: () => import('./pages/quest/quest').then((m) => m.Quest),
   },
 ];
