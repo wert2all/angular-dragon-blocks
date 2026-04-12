a227938c050e31a264e7c7bc8dec3ce1edbd7305
Serhii Shvets <wert2all@gmail.com>
2026-04-12 13:33:07 +0300

    fix: fix map page

--- src/app/pages/map/map-page.component.ts
+++ src/app/pages/map/map-page.component.ts
@@ -6,12 +6,18 @@ import { questFeature } from "../../features/quest/store/quest.feature";
   selector: "app-map-page",
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-      <h1 class="text-3xl font-bold text-deep-purple mb-4">
-        Dragon's World Map
-      </h1>
+<div class="flex justify-center">
+    <div class="items-center justify-center p-12 border-4 border-vibrant-orange bg-warm-cream rounded-2xl ">
+      <img
+        src="/gemini_generated_map.png"
+        alt="Dragon's World Map"
+        class="max-w-full h-auto rounded-lg shadow-lg"
+      />
+    </div>
+    </div>
   `,
 })
 export class MapPageComponent {
   private store = inject(Store);
-  protected quests = this.store.selectSignal(questFeature.selectKidQuests)
+  protected quests = this.store.selectSignal(questFeature.selectKidQuests);
 }
