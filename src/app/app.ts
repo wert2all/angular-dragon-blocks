ab9ceadae6db1f3d6fd818260a337ff336bb4025
Serhii Shvets <wert2all@gmail.com>
2026-04-12 13:33:07 +0300

    fix: fix map page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CenterLayout } from './layout/center-layout';
-import { GameContent } from './layout/game-content';
-import { Header } from './layout/header/header';
-import { Helper } from './features/helper/helper';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { CenterLayout } from "./layout/center-layout";
+import { GameContent } from "./layout/game-content";
+import { Header } from "./layout/header/header";
+import { Helper } from "./features/helper/helper";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [RouterOutlet, CenterLayout, GameContent, Header, Helper],
   template: `
     <app-header />
     <app-center-layout>
-      <app-game-content>
         <router-outlet />
-      </app-game-content>
-    </app-center-layout>
+      </app-center-layout>
     <app-helper />
   `,
 })
-export class App {}
+export class App { }
