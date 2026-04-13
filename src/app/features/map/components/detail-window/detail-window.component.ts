7a8f5f4b24a43b1654c781b2c22d92ad5bb87e38
Serhii Shvets <wert2all@gmail.com>
2026-04-13 14:38:00 +0300

    refactor: extract content from MapDetailWindowComponent

--- src/app/features/map/components/detail-window/detail-window.component.ts
+++ src/app/features/map/components/detail-window/detail-window.component.ts
@@ -1,24 +1,11 @@
-import type { ViewQuestMarker } from '../../../../features/quest/store/quest.types';
-import { ChangeDetectionStrategy, Component, inject, input, output } from '@angular/core';
-import { Router } from '@angular/router';
-import { QuestDetails } from '../../../../features/quest/components/details/details';
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-map-detail-window',
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './detail-window.component.html',
   styleUrl: './detail-window.component.css',
-  imports: [QuestDetails],
 })
 export class MapDetailWindowComponent {
-  readonly quest = input.required<ViewQuestMarker | null>();
-  readonly closeChange = output<void>();
-  private router = inject(Router);
-
-  onWordClick() {
-    const q = this.quest();
-    if (q) {
-      this.router.navigate(['/quest', q.id]);
-    }
-  }
+  readonly closeWindow = output<void>();
 }
