d05a458294c64caae938644598a26d69eb489516
Serhii Shvets <wert2all@gmail.com>
2026-04-13 14:21:58 +0300

    refactor: move coponents to correct namespace

--- src/app/features/quest/components/syllables/syllables.ts
+++ src/app/features/quest/components/syllables/syllables.ts
@@ -1,7 +1,7 @@
 import { Component, input } from '@angular/core';
 import { ViewSyllable } from '../../store/quest.types';
-import { LegoBrick } from '../../../../layout/lego-brick/lego-brick';
-import { BorderLegoBrick } from '../../../../layout/border-lego-brick/border-lego-brick';
+import { BorderLegoBrick } from '../../../../layout/lego/border-lego-brick/border-lego-brick';
+import { LegoBrick } from '../../../../layout/lego/lego-brick/lego-brick';
 
 @Component({
   selector: 'app-quest-syllables',
